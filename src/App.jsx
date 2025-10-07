bf50777a94977aa61eaaf4a67627c78255329dd8
npdtse <npdtse@gmail.com>
2025-10-07 15:17:34 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/upload', {
+      const response = await fetch('https://folhas-backend.onrender.com/upload', {
         method: 'POST',
         body: formData,
       });
